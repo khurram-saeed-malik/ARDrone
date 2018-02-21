854f32e839a01c7319cecb9900e209c19598a4eb
Swagam <s165155@student.dtu.dk>
2018-02-21 12:06:02 +0100

    Added object detection.

--- com/group/1/main.py
+++ com/group/1/main.py
@@ -1,33 +1,90 @@
 import libardrone
-import cv2
-
-
 from time import sleep
+
+import cv2
+cam = cv2.VideoCapture('tcp://192.168.1.1:5555')
+running = True
+while running:
+    # get current frame of video
+    running, frame = cam.read()
+    status = "No Targets"
+
+    if running:
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == 27:
+            # escape key pressed
+            running = False
+    else:
+        # error reading frame
+        print 'error reading video feed'
+
+    if not running:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (7, 7), 0)
+    edged = cv2.Canny(blurred, 50, 150)
+
+    (_, cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # loop over the contours
+    for c in cnts:
+        # approximate the contour
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.01 * peri, True)
+
+        # ensure that the approximated contour is "roughly" rectangular
+        if len(approx) >= 4 and len(approx) <= 6:
+            # compute the bounding box of the approximated contour and
+            # use the bounding box to compute the aspect ratio
+            (x, y, w, h) = cv2.boundingRect(approx)
+            aspectRatio = w / float(h)
+
+            # compute the solidity of the original contour
+            area = cv2.contourArea(c)
+            hullArea = cv2.contourArea(cv2.convexHull(c))
+            solidity = area / float(hullArea)
+
+            # compute whether or not the width and height, solidity, and
+            # aspect ratio of the contour falls within appropriate bounds
+            keepDims = w > 25 and h > 25
+            keepSolidity = solidity > 0.9
+            keepAspectRatio = aspectRatio >= 0.8 and aspectRatio <= 1.2
+
+            # ensure that the contour passes all our tests
+            if keepDims and keepSolidity and keepAspectRatio:
+                # draw an outline around the target and update the status
+                # text
+                cv2.drawContours(frame, [approx], -1, (0, 0, 255), 4)
+                status = "Target(s) Acquired"
+
+                # compute the center of the contour region and draw the
+                # crosshairs
+                M = cv2.moments(approx)
+                (cX, cY) = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+                (startX, endX) = (int(cX - (w * 0.15)), int(cX + (w * 0.15)))
+                (startY, endY) = (int(cY - (h * 0.15)), int(cY + (h * 0.15)))
+                cv2.line(frame, (startX, cY), (endX, cY), (0, 0, 255), 3)
+                cv2.line(frame, (cX, startY), (cX, endY), (0, 0, 255), 3)
+
+    # draw the status text on the frame
+    cv2.putText(frame, status, (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                (0, 0, 255), 2)
+
+    # show the frame and record if a key is pressed
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+cam.release()
+cv2.destroyAllWindows()
+
 drone = libardrone.ARDrone()
-cap = cv2.VideoCapture()
-cap.open('tcp://192.168.1.1:5555')
+
 drone.takeoff()
 sleep(3)
 drone.hover()
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-
-
-
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-sleep(3)
+sleep(5)
 drone.land()
 sleep(3)
 drone.halt()
 
-cap.release()
-cv2.destroyAllWindows()
