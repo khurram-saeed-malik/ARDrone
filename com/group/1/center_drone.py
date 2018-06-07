3e08525e4b12a3a698c02e56c3781663226eb958
mortentang <mortentc@hotmail.com>
2018-06-07 11:30:02 +0200

    Display drone camera

--- com/group/1/center_drone.py
+++ com/group/1/center_drone.py
@@ -1,29 +1,47 @@
 # coding=utf-8
 from time import sleep
+from lib import libardrone
 
 
-def allign(drone, x, y, w, h):
+drone = libardrone.ARDrone()
+
+
+def allign(x,y,w,h):
+
     if x < 300:
         print("QR is right to the center, moving left")
         drone.move_left()
         sleep(0.01)
         drone.hover()
-    #  sleep(2)
+      #  sleep(2)
 
     if x > 340:
         print("QR is left to the center, moving right")
         drone.move_right()
         sleep(0.01)
         drone.hover()
-    # sleep(2)
+       # sleep(2)
 
-    if 300 < x < 340:
+    if 300 < x < 340 and 140 < y < 220:
         print("QR is alligned")
         drone.hover()
-        #  sleep(2)
+      #  sleep(2)
         drone.land()
 
+    if y < 140:
+        print("QR is under the center, moving up")
+        drone.move_up()
+        sleep(0.01)
+        drone.hover()
+      #  sleep(2)
+
+    if y > 220:
+        print("QR is over the center, moving down")
+        drone.move_down()
+        sleep(0.01)
+        drone.hover()
+    #  sleep(2)
 
 class center_drone(object):
     def __init__(self):
-        print("Object detection: Class loaded")
+     print("Object detection: Class loaded")
