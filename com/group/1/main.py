6590e9b7dc48fa03e612f00b73402eabcf0839d3
khurramsaeed <s165162@student.dtu.dk>
2018-06-11 04:33:51 -0700

    Autonomus drone detection and flying

--- com/group/1/main.py
+++ com/group/1/main.py
@@ -7,27 +7,12 @@ import capture
 import object_detection
 
 drone = libardrone.ARDrone()
-# 'tcp://192.168.1.1:5555'
-cam = cv2.VideoCapture(0)
+cam = cv2.VideoCapture('tcp://192.168.1.1:5555')
 
 
 # main method declaration
 def main():
-    try:
-        thread.start_new_thread(flying(), ())
-        thread.start_new_thread(video(), ())
-    except:
-        print 'Error> Unable to start threads..'
-
-
-def flying():
-    #drone.takeoff()
-    #sleep(4)
-    object_detection.detect(cam, drone, 10)
-
-
-def video():
-    capture.video_feed(cam, drone)
+    object_detection.detect(cam)
 
 
 # main method entry point
